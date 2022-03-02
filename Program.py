2053476c53e8c8cb339e81faa6dd65c0e5e744bd
dmitriyshub <d.shtranvasser@gmail.com>
2022-03-02 12:29:28 +0200

    adding program.py file

---
+++ Program.py
@@ -0,0 +1,17 @@
+#One line comment
+'''
+Multi line
+comment
+'''
+print('Hello World!')
+
+x = float(input("Enter a number for x:"))
+#print(x)
+
+y = float(input("Enter a number for y:"))
+
+z = x + y
+
+print("Solution: " , z );
+
+print("calculation: {} + {} = {}".format(x,y, x+y))
